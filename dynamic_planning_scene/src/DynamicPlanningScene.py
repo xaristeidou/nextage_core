665dbc1a3416253f355ad93ec0eaec2818b346bc
Christoforos Aristeidou <aristeidouxristos@gmail.com>
2024-04-04 17:30:25 +0300

    modified code to fit new hobs

--- dynamic_planning_scene/src/DynamicPlanningScene.py
+++ dynamic_planning_scene/src/DynamicPlanningScene.py
@@ -8,7 +8,7 @@ from moveit_msgs.msg import ObjectColor
 from moveit_msgs.srv import ApplyPlanningScene
 from geometry_msgs.msg import PoseStamped,TransformStamped
 
-local_mesh_repo_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+local_mesh_repo_dir = os.path.dirname(os.path.dirname((__file__)))
 # local_mesh_repo_dir = os.path.dirname(__file__)
 
 
@@ -21,12 +21,12 @@ class DynamicPlanningScene:
         self.tf_buffer = tf2_ros.Buffer()
         self.listener = tf2_ros.TransformListener(self.tf_buffer)
         self.colors_dict = dict()
-        self.tf_names_list = ['big_hob_frame','small_hob_frame','pcb_frame','double_hob_frame']
+        self.tf_names_list = ['small_hob_frame','medium1_hob_frame','medium2_hob_frame','big_hob_frame','pcb_frame']
 
 
     def check_transforms(self,tf_frame_name):
         try:
-            _ = self.tf_buffer.lookup_transform("world", tf_frame_name, rospy.Time(0))
+            _ = self.tf_buffer.lookup_transform("base_link", tf_frame_name, rospy.Time(0))
             rospy.loginfo("Frame {} is being published.".format(tf_frame_name))
             return True
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
@@ -36,37 +36,45 @@ class DynamicPlanningScene:
     def publish_planning_scene(self):
 
         
-        if self.check_transforms('big_hob_frame'):
-            big_hob = PoseStamped()
-            big_hob.header.frame_id = "big_hob_frame"
-            big_hob.pose.orientation.x, big_hob.pose.orientation.y, big_hob.pose.orientation.z, big_hob.pose.orientation.w = 0.0, 0.0, 0.0, 1.0
-            big_hob.pose.position.x, big_hob.pose.position.y, big_hob.pose.position.z = 0.0,0.0,0.0 
-            self.planning_scene_interface.add_mesh("big_hob", big_hob, os.path.join(local_mesh_repo_dir,'elux_robot_ros/elux_ur10e_description/meshes/big_hob/big_hob_pivot_in_center.stl'))
-            self.setColor(name="big_hob",r=0.5,g=0.5,b=0.5,a=0.7)
-            self.sendColors()
         if self.check_transforms('small_hob_frame'):
             small_hob = PoseStamped()
             small_hob.header.frame_id = "small_hob_frame"
             small_hob.pose.orientation.x, small_hob.pose.orientation.y, small_hob.pose.orientation.z, small_hob.pose.orientation.w = 0.0, 0.0, 0.0, 1.0
-            small_hob.pose.position.x, small_hob.pose.position.y, small_hob.pose.position.z = 0.0,0.0,.0
-            self.planning_scene_interface.add_mesh("small_hob", small_hob, os.path.join(local_mesh_repo_dir,'elux_robot_ros/elux_ur10e_description/meshes/small_hob/small_hob_pivot_in_center.stl'))
+            small_hob.pose.position.x, small_hob.pose.position.y, small_hob.pose.position.z = 0.0,0.0,0.0 
+            self.planning_scene_interface.add_mesh("small_hob", small_hob, os.path.join(local_mesh_repo_dir,'meshes/small_hob_pivot_in_center.stl'))
             self.setColor(name="small_hob",r=0.5,g=0.5,b=0.5,a=0.7)
             self.sendColors()
-        if self.check_transforms('pcb_frame'):
-            pcb = PoseStamped()
-            pcb.header.frame_id = "pcb_frame"
-            pcb.pose.orientation.x, pcb.pose.orientation.y, pcb.pose.orientation.z, pcb.pose.orientation.w = 0.0, 0.0, 0.0, 1.0
-            pcb.pose.position.x, pcb.pose.position.y, pcb.pose.position.z = .0,.0,.0
-            self.planning_scene_interface.add_mesh("pcb", pcb, os.path.join(local_mesh_repo_dir,'elux_robot_ros/elux_ur10e_description/meshes/tool_box.stl'))
-            self.setColor(name="pcb",r=.0,g=.0,b=.0,a=0.7)
+        if self.check_transforms('medium1_hob_frame'):
+            medium1_hob = PoseStamped()
+            medium1_hob.header.frame_id = "medium1_hob_frame"
+            medium1_hob.pose.orientation.x, medium1_hob.pose.orientation.y, medium1_hob.pose.orientation.z, medium1_hob.pose.orientation.w = 0.0, 0.0, 0.0, 1.0
+            medium1_hob.pose.position.x, medium1_hob.pose.position.y, medium1_hob.pose.position.z = 0.0,0.0,.0
+            self.planning_scene_interface.add_mesh("medium1_hob", medium1_hob, os.path.join(local_mesh_repo_dir,'meshes/small_hob_pivot_in_center.stl'))
+            self.setColor(name="medium1_hob",r=0.5,g=0.5,b=0.5,a=0.7)
             self.sendColors()
-        if self.check_transforms('double_hob_frame'):
-            double_hob = PoseStamped()
-            double_hob.header.frame_id = "double_hob_frame"
-            double_hob.pose.orientation.x, double_hob.pose.orientation.y, double_hob.pose.orientation.z, double_hob.pose.orientation.w = 0.0, 0.0, 0.0, 1.0
-            double_hob.pose.position.x, double_hob.pose.position.y, double_hob.pose.position.z = .0,.0,.0
-            self.planning_scene_interface.add_mesh("double_hob", double_hob, os.path.join(local_mesh_repo_dir,'elux_robot_ros/elux_ur10e_description/meshes/mini_plate/mini_plate.stl'))
-            self.setColor(name="double_hob",r=0.5,g=0.5,b=0.5,a=0.7)
+        if self.check_transforms('medium2_hob_frame'):
+            medium2_hob = PoseStamped()
+            medium2_hob.header.frame_id = "medium2_hob_frame"
+            medium2_hob.pose.orientation.x, medium2_hob.pose.orientation.y, medium2_hob.pose.orientation.z, medium2_hob.pose.orientation.w = 0.0, 0.0, 0.0, 1.0
+            medium2_hob.pose.position.x, medium2_hob.pose.position.y, medium2_hob.pose.position.z = .0,.0,.0
+            self.planning_scene_interface.add_mesh("medium2_hob", medium2_hob, os.path.join(local_mesh_repo_dir,'meshes/small_hob_pivot_in_center.stl'))
+            self.setColor(name="medium2_hob",r=0.5,g=0.5,b=0.5,a=0.7)
+            self.sendColors()
+        if self.check_transforms('big_hob_frame'):
+            big_hob = PoseStamped()
+            big_hob.header.frame_id = "big_hob_frame"
+            big_hob.pose.orientation.x, big_hob.pose.orientation.y, big_hob.pose.orientation.z, big_hob.pose.orientation.w = 0.0, 0.0, 0.0, 1.0
+            big_hob.pose.position.x, big_hob.pose.position.y, big_hob.pose.position.z = .0,.0,.0
+            self.planning_scene_interface.add_mesh("big_hob", big_hob, os.path.join(local_mesh_repo_dir,'meshes/small_hob_pivot_in_center.stl'))
+            self.setColor(name="big_hob",r=0.5,g=0.5,b=0.5,a=0.7)
+            self.sendColors()
+        if self.check_transforms('pcb_frame'):
+            big_hob = PoseStamped()
+            big_hob.header.frame_id = "pcb_frame"
+            big_hob.pose.orientation.x, big_hob.pose.orientation.y, big_hob.pose.orientation.z, big_hob.pose.orientation.w = 0.0, 0.0, 0.0, 1.0
+            big_hob.pose.position.x, big_hob.pose.position.y, big_hob.pose.position.z = .0,.0,.0
+            self.planning_scene_interface.add_mesh("pcb", big_hob, os.path.join(local_mesh_repo_dir,'meshes/pcb.stl'))
+            self.setColor(name="pcb",r=0.5,g=0.5,b=0.5,a=0.7)
             self.sendColors()
 
     def setColor(self, name, r, g, b, a=0.9):
